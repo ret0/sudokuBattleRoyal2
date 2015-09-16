19caa6803a2b66257aaa7e5ceceb9b7cf53bf4bc
Reto Kleeb <reto.kleeb@elca.ch>
2015-09-16 16:34:28 +0200

    impl according to trello

--- src/main/java/ch/elca/students/sudokubattleroyal2/config/WebSecurityConfig.java
+++ src/main/java/ch/elca/students/sudokubattleroyal2/config/WebSecurityConfig.java
@@ -1,21 +1,31 @@
 package ch.elca.students.sudokubattleroyal2.config;
 
+import ch.elca.students.sudokubattleroyal2.game.GameManager;
+import com.google.common.collect.ImmutableList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private GameManager gameManager;
+
+    private Set<String> activeUsers = Collections.synchronizedSet(new HashSet<>());
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -36,17 +46,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(new AuthenticationProvider() {
-            @Override
-            public boolean supports(Class<?> authentication) {
-                return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
-            }
+        auth.authenticationProvider(formLoginBasedAuthenticationProvider());
+    }
 
-            @Override
-            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-                UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
-                return new UsernamePasswordAuthenticationToken(token.getName(), token.getCredentials());
+    @Bean
+    public AuthenticationProvider formLoginBasedAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(username -> {
+            if (activeUsers.contains(username) || gameManager.isGameStarted()) {
+                throw new UsernameNotFoundException("user already logged in, or game is in progress");
+            } else {
+                activeUsers.add(username);
+                return new User(username, "", ImmutableList.of());
             }
         });
+        return daoAuthenticationProvider;
     }
+
 }
