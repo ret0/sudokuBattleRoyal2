220e08ecb2259bbb2260c9a85245b959b5640413
Reto Kleeb <reto.kleeb@elca.ch>
2015-09-21 17:46:38 +0200

    offering findAll method in playermanager to allow the consistent usage of it

--- src/main/java/ch/elca/students/sudokubattleroyal2/game/PlayerManager.java
+++ src/main/java/ch/elca/students/sudokubattleroyal2/game/PlayerManager.java
@@ -5,6 +5,8 @@ import ch.elca.students.sudokubattleroyal2.persistence.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 
 @Component
 public class PlayerManager {
@@ -14,6 +16,13 @@ public class PlayerManager {
 
     public void add(Player player) {
         playerRepository.save(player);
+    }
+
+    /**
+     * @return all Player objects
+     */
+    public List<Player> findAll() {
+        return playerRepository.findAll();
     }
 
 }
