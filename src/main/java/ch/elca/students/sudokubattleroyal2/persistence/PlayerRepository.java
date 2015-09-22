55ecfeefd7a3c89bc83ca7e165cb21b548075cae
mkempf <martin.kempf@gmail.com>
2015-09-22 13:48:47 +0200

    updated comment in the playerrepo

--- src/main/java/ch/elca/students/sudokubattleroyal2/persistence/PlayerRepository.java
+++ src/main/java/ch/elca/students/sudokubattleroyal2/persistence/PlayerRepository.java
@@ -4,9 +4,12 @@ import ch.elca.students.sudokubattleroyal2.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * @author rkb
+ * Repository that offers CRUD operations for Player Entities
  */
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
+    /**
+     * @return a single Player object with that name, if there exists such a player, null otherwise.
+     */
     Player findOneByPlayerName(String playerName);
 }
